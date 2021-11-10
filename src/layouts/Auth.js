fa2bc0a744d469f79ed9a7b6fb04c77d0c4d1450
Ibad Ur Rehman <56487132+me-ibad@users.noreply.github.com>
2021-11-11 03:38:28 +0500

    Initial commit

---
+++ src/layouts/Auth.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+// components
+
+import Navbar from 'components/Navbars/IndexNavbar.js';
+import FooterSmall from 'components/Footers/FooterSmall.js';
+
+// views
+
+import Login from 'views/auth/Login.js';
+import Register from 'views/auth/Register.js';
+
+export default function Auth() {
+  return (
+    <>
+      <Navbar transparent />
+      <main>
+        <section className='relative w-full h-full py-20 min-h-screen'>
+          <div
+            className='absolute top-0 w-full h-full bg-white-800 bg-no-repeat bg-full bg-fixed'
+            style={{
+              backgroundImage:
+                'url(' + require('assets/img/bottom.png').default + ')',
+            }}
+          ></div>
+          <Switch>
+            <Route path='' exact component={Login} />
+            <Route path='/auth/register' exact component={Register} />
+            <Redirect from='/auth' to='/auth/login' />
+          </Switch>
+          <FooterSmall absolute />
+        </section>
+      </main>
+    </>
+  );
+}
---
+++ src/views/auth/Login.js
@@ -0,0 +1,113 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Login() {
+  return (
+    <>
+      <div className='container mx-auto px-4 h-full'>
+        <div className='flex content-center items-center justify-center h-full'>
+          <div className='w-full lg:w-4/12 px-4'>
+            <div className='relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200 border-0'>
+              <div className='rounded-t mb-0 px-6 py-6'>
+                <div className='flex grid content-center items-center justify-center '>
+                  <img
+                    alt='...'
+                    className='w-full h-full mr-1'
+                    src={require('assets/img/bg2.png').default}
+                  />
+                </div>
+                <hr className='mt-6 border-b-1 border-blueGray-300' />
+              </div>
+              <div className='flex-auto px-4 lg:px-10 py-10 pt-0'>
+                <div className='flex text-black mb-3 font-bold'>
+                  <div>Version: &nbsp;</div>
+                  <label class='switch'>
+                    <input type='checkbox' id='togBtn' />
+                    <div class='slider round'>
+                      <span class='on'>Alumons</span>
+                      <span class='off'>Prueba</span>
+                    </div>
+                  </label>
+                </div>
+                <form>
+                  <div className='relative w-full mb-3'>
+                    <div class='relative flex w-full flex-wrap items-stretch mb-3'>
+                      <span class='z-10 h-full leading-snug font-normal absolute text-center text-black absolute bg-transparent rounded text-base items-center justify-center w-8 pl-3 py-3'>
+                        <i class='fas fa-user'></i>
+                      </span>
+                      <input
+                        type='text'
+                        placeholder='Email'
+                        class='px-3 py-3 placeholder-blueGray-800 text-blueGray-600 relative bg-white bg-white rounded text-sm shadow outline-none focus:outline-none focus:shadow-outline w-full pl-10'
+                      />
+                    </div>
+                  </div>
+
+                  <div className='relative w-full mb-3'>
+                    {/* <label
+                      className='block uppercase text-blueGray-600 text-xs font-bold mb-2'
+                      htmlFor='grid-password'
+                    >
+                      Password
+                    </label> */}
+                    <div class='relative flex w-full flex-wrap items-stretch mb-3'>
+                      <span class='z-10 h-full leading-snug font-normal absolute text-center text-black absolute bg-transparent rounded text-base items-center justify-center w-8 pl-3 py-3'>
+                        <i class='fas fa-phone'></i>
+                      </span>
+                      <input
+                        type='text'
+                        placeholder='Telefono'
+                        class='px-3 py-3 placeholder-blueGray-800 text-blueGray-600 relative bg-white bg-white rounded text-sm shadow outline-none focus:outline-none focus:shadow-outline w-full pl-10'
+                      />
+                    </div>
+                  </div>
+                  <div>
+                    <label className='inline-flex items-center cursor-pointer'>
+                      <input
+                        id='customCheckLogin'
+                        type='checkbox'
+                        className='form-checkbox border-0 rounded text-blueGray-700 ml-1 w-5 h-5 ease-linear transition-all duration-150'
+                      />
+                      <span className='ml-2 text-sm font-semibold text-blueGray-600'>
+                        Remember me
+                      </span>
+                    </label>
+                  </div>
+
+                  <div className='text-center'>
+                    <button
+                      className='text-white  text-sm font-bold uppercase px-6 py-3  outline-none focus:outline-none  '
+                      type='button'
+                    >
+                      <img
+                        alt='...'
+                        className='w-full h-full mr-1'
+                        src={require('assets/img/Entrar.png').default}
+                      />
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+            <div className='flex flex-wrap mt-6 relative'>
+              <div className='w-1/2'>
+                <a
+                  href='#pablo'
+                  onClick={(e) => e.preventDefault()}
+                  className='text-black'
+                >
+                  <small>Forgot password?</small>
+                </a>
+              </div>
+              <div className='w-1/2 text-right'>
+                <Link to='' className='text-black'>
+                  <small>Create new account</small>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
